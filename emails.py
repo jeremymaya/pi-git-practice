d8dfb1d563d4773288476935e199dc3a91922fe1
Kyungrae Kim <jeremiah.kim@hotmail.com>
2020-06-11 10:35:05 -0700

    .gitignore and emails.py added

---
+++ emails.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import sys
+import email
+import smtplib
+
+def generate_alert_email(sender, recipient, subject, body, attachment_path):
+    message = email.message.EmailMessage()
+    message["From"] = sender
+    message["To"] = recipient
+    message["Subject"] = subject
+    message.set_content(body)
+
+    return message
+
+def send_email(message):
+    mail_server = smtplib.SMTP('localhost')
+    mail_server.send_message(message)
+    mail_server.quit()
