ea7b385ceb0c15a6332fc15f4e6b9a8eabb2be2c
Kyungrae Kim <jeremiah.kim@hotmail.com>
2020-06-11 22:24:56 -0700

    emails.oy fixed to include SMTP server info and login + README.md updated

--- emails.py
+++ emails.py
@@ -3,6 +3,12 @@
 import sys
 import email
 import smtplib
+from decouple import config
+
+SERVER = config("SMTP_SERVER")
+PORT = config("SMTP_PORT")
+USERNAME = config("USERNAME")
+PASSWORD = config("PASSWORD")
 
 def generate_alert_email(sender, recipient, subject, body):
     message = email.message.EmailMessage()
@@ -14,6 +20,7 @@ def generate_alert_email(sender, recipient, subject, body):
     return message
 
 def send_email(message):
-    mail_server = smtplib.SMTP('localhost')
+    mail_server = smtplib.SMTP_SSL(SERVER, port=PORT)
+    mail_server.login(USERNAME, PASSWORD)
     mail_server.send_message(message)
     mail_server.quit()
--- system_health_check.py
+++ system_health_check.py
@@ -11,19 +11,19 @@ RECIPIENT = config("RECIPIENT")
 
 def check_temperature():
     measure_temp = os.popen("vcgencmd measure_temp").readline()
-    temp = re.findall('\d*\.?\d+', measure_temp)
+    temp = re.findall(r'\d*\.?\d+', measure_temp)
     return float(temp[0]) < 80.0
 
 def send_alert(subject):
     sender = SENDER
     recipient = RECIPIENT
-    body = emails.generate_alert_email(SENDER, RECIPIENT, subject, body)
+    body = "test"
+    message = emails.generate_alert_email(sender, recipient, subject, body)
     emails.send_email(message)
 
 def main(argv):
-    if not check_temperature():
-        sibject = "Alert - The core temperature is over 80.0'C"
-        send_alert(subject)
+    subject = "Alert - The core temperature is over 80.0'C"
+    send_alert(subject)
 
 if __name__ == "__main__":
     main(sys.argv)
